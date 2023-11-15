491331d03d639909b3e4d7c6529160a7d5b13434
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2023-11-15 21:33:20 +0100

    updating packer

--- lua/kaaf/packer.lua
+++ lua/kaaf/packer.lua
@@ -1,7 +1,17 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   use {
@@ -35,4 +45,7 @@ return require('packer').startup(function(use)
     use 'saadparwaiz1/cmp_luasnip'
     use 'rafamadriz/friendly-snippets'
 
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
