592abc783f2771c9b5414941261b863bfcd69e18
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2024-06-07 19:15:52 +0200

    Added indentline highlight so I don't lose myself

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    configure =function ()
+    config =function ()
         require 'nvim-treesitter.install'.compilers = { vim.fn.getenv("CC"), "gcc, clang" }
 
         require'nvim-treesitter.configs'.setup {
